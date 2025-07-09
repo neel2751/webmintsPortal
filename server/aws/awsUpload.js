c97f24d640e5463a528f146b5714ab2cda2c33f2
Neel Patel <neel@Neels-MBP.lan>
2025-07-09 09:34:21 +0100

    changed the access key

--- server/aws/awsUpload.js
+++ server/aws/awsUpload.js
@@ -15,7 +15,7 @@ const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_ID,
-    secretAccessKey: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACCESS_PORTAL_KEY,
   },
 });
 
